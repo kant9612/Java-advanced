de430dd194b8668ef24b6badc09fb3d0fce7a69a
kant9612 <kanstephentakayama@gmail.com>
2021-05-06 05:21:09 +0000

    メソッドを追加できる

--- 応用02/Dessert.java
+++ 応用02/Dessert.java
@@ -1,10 +1,40 @@
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE;
+
+	APPLE(100){
+		void advertise() {
+			System.out.println("真っ赤な林檎が" + getPrice() + "円！");
+		}
+	},
+
+	ICE_CREAM(200){
+		void advertise() {
+			System.out.println("夏にぴったりアイスクリームが" + getPrice() + "円！");
+		}
+	},
+
+	CAKE(300){
+		void advertise() {
+			System.out.println("皆大好き甘いケーキが" + getPrice() + "円！");
+		}
+	},
+	;
+
+	private int price;
+
+	private Dessert(int price) {
+		this.price = price;
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+	abstract void advertise();
 
 	public static void main(String[] args) {
-		System.out.println(Dessert.APPLE);
-		System.out.println(Dessert.ICE_CREAM);
-		System.out.println(Dessert.CAKE);
+		Dessert.APPLE.advertise();
+		Dessert.ICE_CREAM.advertise();
+		Dessert.CAKE.advertise();
 	}
 }
