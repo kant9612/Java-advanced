30369c0dc244ac978bf09b523aedd03065c3cad3
kant9612 <kanstephentakayama@gmail.com>
2021-05-06 05:03:56 +0000

    列挙型(enum)とは

---
+++ 応用02/Dessert.java
@@ -0,0 +1,10 @@
+
+public enum Dessert {
+	APPLE, ICE_CREAM, CAKE;
+
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE);
+		System.out.println(Dessert.ICE_CREAM);
+		System.out.println(Dessert.CAKE);
+	}
+}
