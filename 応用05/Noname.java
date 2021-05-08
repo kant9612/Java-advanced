99f5493c401a31000faa87026f8b41a82bf1c03c
kant9612 <kanstephentakayama@gmail.com>
2021-05-08 03:08:11 +0000

    無名クラスにつite exit

---
+++ 応用05/Noname.java
@@ -0,0 +1,12 @@
+
+public class Noname {
+	
+	public String hello() {
+		return "Hello A!";
+	}
+	
+	public static void main(String[] args) {
+		Noname noname = new Noname();
+		System.out.println(noname.hello());
+	}
+}
