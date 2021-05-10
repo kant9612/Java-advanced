fc85633120e9e97ddd07a738d1e5dfa16b6e259e
kant9612 <kanstephentakayama@gmail.com>
2021-05-10 01:21:17 +0000

    ストリーム（ラムダ式を使わず)

--- 応用06/Greeting.java
+++ 応用06/Greeting.java
@@ -2,13 +2,13 @@ package ouyou06;
 
 @FunctionalInterface
 public interface Greeting {
-	
+
 	public String hello();
-	
+
 	Greeting greeting = () -> {
 		return "こんにちは";
 	};
-	
+
 	public static void sayHello(Greeting g) {
 		System.out.println(g.hello());
 	}
