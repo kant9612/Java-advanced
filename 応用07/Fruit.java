9e71546d5c67e29a1e24a5a55432913e619472ae
kant9612 <kanstephentakayama@gmail.com>
2021-05-11 04:41:06 +0000

    復習のためコメント付与

--- 応用07/Fruit.java
+++ 応用07/Fruit.java
@@ -5,19 +5,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Fruit {
+	//名前
 	public String name;
-
+	//在庫数
 	public int quantity;
 
+	//名前と在庫数を引数にしたコンストラクタ
 	public Fruit(String name, int quantity) {
 		this.name = name;
 		this.quantity = quantity;
 	}
 
+	//在庫数を増やしたフルーツを返すメソッド
 	public Fruit order(int quantityToAdd) {
 		return new Fruit(this.name, this.quantity + quantityToAdd);
 	}
 
+	//出力数にtoStringをオーバーライド
 	@Override
 	public String toString() {
 		return name + ": " + quantity + "個";
